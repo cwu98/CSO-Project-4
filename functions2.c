74a68f95f23f70f0017d8f5f65e4f6af390b8830
Joanna <joannakl@cs.nyu.edu>
2019-04-21 20:40:38 +0000

    fun1 works

--- functions2.c
+++ functions2.c
@@ -3,15 +3,28 @@
 #include "given/functions2.h"
 
 // IMPLEMENT FUNCTIONS IN THIS FILE
+/* Author: Cary Wu
+ * Description: implementations of two functions based on disassembled versions of the functions2.o object file
+ */
 
 
 long fun1 ( data a[], int size ){
-
+  int x ;
+  long val = 0;
+  for(x=0;x<size;x++){
+    val = val+a[x].i;
+}
+  return val;
 }
 
-
-
 char* fun2 ( data a[], int size ){
-
+  printf("start of fun2\n");
+  char *foo = fun2(a,size+1);
+  *(foo+size)='\0'; //end of string
+  int j=0;
+  for(j=0;j<size;j++){
+    *(foo+j)=a[j].c;
+  }
+  return foo;
 
 }
